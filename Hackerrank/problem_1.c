9d1ec970e95306dee1b50fa0fb6fd923cd44fcae
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-03-08 22:52:49 +0530

    Hackerrank solved programs

---
+++ Hackerrank/problem_1.c
@@ -0,0 +1,14 @@
+/*https://www.hackerrank.com/challenges/hello-world-c/problem*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+    printf("Hello, World!\n%s",s);
+    return 0;
+}
