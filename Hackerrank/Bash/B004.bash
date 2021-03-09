09dc5c085e907f6514f1f78b15f3bf5f3de0f3e7
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-03-09 22:37:57 +0530

    Hackerrank Solved problems

---
+++ Hackerrank/Bash/B004.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+//https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+for((i=1;i<=50;i++))
+do
+    echo "$i"
+done
+
