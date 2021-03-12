9b9e4a0fe2005c3876329ecc40bee2758559beec
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-03-12 21:35:36 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Bash/B009.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+//https://www.hackerrank.com/challenges/bash-tutorials---arithmetic-operations/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+read line
+printf "%.3f" $(echo "scale=4; $line"|bc)
