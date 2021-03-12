2074339ee0cbfff2d3cd978347ff48e23847ef85
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-03-12 21:01:30 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Bash/B007.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+//https://www.hackerrank.com/challenges/bash-tutorials---getting-started-with-conditionals/problem
+
+read character
+
+case $character in
+  Y|y)echo "YES";;
+  N|n)echo "NO" ;;
+  esac
+
