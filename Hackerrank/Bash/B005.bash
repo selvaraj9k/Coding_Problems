35735d9319a53d055e8f258e0e9e0dbe5a52d1cb
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-03-12 11:46:32 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Bash/B005.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+//https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+
+read X;
+read Y;
+
+sum=$(($X+$Y))
+difference=$(($X-$Y))
+product=$(($X*$Y))
+quotient=$(($X/$Y))
+
+echo "$sum"
+echo "$difference"
+echo "$product"
+echo "$quotient"
