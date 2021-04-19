78bfe4ebf866c652acec09ddc8294656ad4afd1f
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2021-04-19 17:32:32 +0530

    Hacker Solved Problems

--- Hackerrank/SQL/S001.sql
+++ Hackerrank/SQL/S001.sql
@@ -1,3 +1,3 @@
 'https://www.hackerrank.com/challenges/revising-the-select-query/problem'
 
-SELECT * FROM CITY WHERE Population>100000 AND countrycode="USA";
+SQL> SELECT * FROM CITY WHERE Population>100000 AND countrycode="USA";
