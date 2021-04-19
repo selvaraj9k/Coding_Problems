79109f0cc71627b824f0a3c4939461ed9c40fcd4
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-04-19 14:19:31 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/SQL/S005.sql
@@ -0,0 +1,3 @@
+'https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen'
+
+SELECT * FROM CITY WHERE COUNTRYCODE='JPN';
