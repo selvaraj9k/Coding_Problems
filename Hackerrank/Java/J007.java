77fcb3791542b79fb509fca5f129ed64718b46d9
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-07 20:41:40 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Java/J007.java
@@ -0,0 +1,14 @@
+/*https://www.hackerrank.com/challenges/java-end-of-file/problem*/
+
+import java.util.*;
+
+public class J007 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        for(int i=1;s.hasNext()==true;i++)
+        {
+           System.out.println(i+" "+s.nextLine());
+        }
+    }
+}
