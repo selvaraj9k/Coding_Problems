d2a530c6781cd39a5f06b334f205c5d485cf99c5
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-15 08:58:38 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/C/C010.c
@@ -0,0 +1,43 @@
+/*
+ https://www.hackerrank.com/challenges/students-marks-sum/problem
+ */
+#include <stdio.h>
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+    int sum=0,i;
+  switch(gender)
+  {
+      case 'b':
+            for(i=0;i<number_of_students;i=i+2)
+            {
+                sum=sum+marks[i];
+            }
+            break;
+     case 'g':
+            for(i=1;i<number_of_students;i=i+2)
+            {
+                sum=sum+marks[i];
+            }
+            break;
+  }
+  return sum;
+}
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+
+    return 0;
+}
