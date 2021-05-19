093ba34599f0cebfe2523b4580f2d6c73381f7f6
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-19 13:49:35 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/10 Days of Javascript/Day 0/Hello, World.js
@@ -0,0 +1,9 @@
+/*
+ * https://www.hackerrank.com/domains/tutorials/10-days-of-javascript
+ */
+function greeting(parameterVariable)
+{
+    console.log('Hello, World!');
+    process.stdout.write(parameterVariable);
+
+}
