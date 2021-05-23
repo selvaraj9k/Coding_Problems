cea9bd5e84fe061e8036fcaabb9df9f9718eff61
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-24 00:13:01 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/C++/CPP0002.cpp
@@ -0,0 +1,17 @@
+/*
+ * https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    int sum;
+    cin >>a>>b>>c;
+    sum=a+b+c;
+    cout <<sum;
+    return 0;
+}
+
