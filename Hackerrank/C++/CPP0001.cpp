a00cac78e1fe7d4618d254160405a38c686cb0cc
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-24 00:06:23 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/C++/CPP0001.cpp
@@ -0,0 +1,13 @@
+/*
+  https://www.hackerrank.com/challenges/cpp-hello-world/problem
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //printf("Hello, World!");
+    cout<<"Hello, World!";
+    return 0;
+}
+
