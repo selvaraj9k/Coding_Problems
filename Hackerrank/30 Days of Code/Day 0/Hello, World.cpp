f33be84c9e2f4c78c9467da41a536a53e1c4b39e
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-13 18:59:24 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/30 Days of Code/Day 0/Hello, World.cpp
@@ -0,0 +1,18 @@
+/*
+ * https://www.hackerrank.com/challenges/30-hello-world/problem
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string input_string;
+    getline(cin, input_string);
+    cout << "Hello, World." << endl;
+    cout << input_string <<endl;
+    return 0;
+}
