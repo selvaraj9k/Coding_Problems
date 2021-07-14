a0bc947605e09e088b11818f65ed395e8f5824fb
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-14 19:46:48 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/30 Days of Code/Day 1/Data Types.cpp
@@ -0,0 +1,25 @@
+/*
+ * https://www.hackerrank.com/challenges/30-data-types/problem
+ */
+
+nclude <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int i1;
+    double d1;
+    string s1;
+    cin>>i1>>d1;
+    cin.ignore();
+    getline(cin>>ws,s1);
+    cout<<i+i1<<endl;
+    cout<<fixed<<setprecision(1)<<d+d1<<endl;
+    cout<<s<<s1;
+    return 0;
+}
