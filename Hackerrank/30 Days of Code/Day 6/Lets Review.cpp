0d64feb314fd2a41dbcf1081499f39a652c9f15e
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-19 18:22:18 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/30 Days of Code/Day 6/Lets Review.cpp
@@ -0,0 +1,28 @@
+/*
+ * https://www.hackerrank.com/challenges/30-review-loop/problem
+ */
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i,j;
+    cin >>n;
+    for(i=0;i<n;i++)
+    {
+       string s,s1,s2;
+        cin >>s;
+        for(j=0;j<s.size();j++)
+        {
+            if(j%2==0)
+            {
+            s1+=s[j];
+            }
+            else
+            {
+            s2+=s[j];
+            }
+        }
+        cout <<s1<<" "<<s2<<endl;
+    }
+    return 0;
+}
+
