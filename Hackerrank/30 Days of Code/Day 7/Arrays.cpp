994345370b811c610f2e4c5212e2b4c86448b121
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-22 13:10:03 +0530

    Hakcerrank Solved Problems

--- Hackerrank/30 Days of Code/Day 7/Arrays.cpp
+++ Hackerrank/30 Days of Code/Day 7/Arrays.cpp
@@ -2,7 +2,20 @@
  * https://www.hackerrank.com/challenges/30-arrays/problem
  */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
    for(int i=n-1;i>=0;i--)
     {
         cout<<arr[i]<<" ";
     }
+   return 0;
+}
--- Hackerrank/30 Days of Code/Day 9/Recursion 3.cpp
+++ Hackerrank/30 Days of Code/Day 9/Recursion 3.cpp
@@ -2,9 +2,17 @@
  * https://www.hackerrank.com/challenges/30-recursion/problem
  */
 
+#include<iostream>
 int factorial(int n) {
     if(n==1)
     return n;
     else
    return n*factorial(n-1);
 }
+int main() {
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
+
