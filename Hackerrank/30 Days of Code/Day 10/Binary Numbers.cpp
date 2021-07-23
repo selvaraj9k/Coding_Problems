23c0d6ce32f2a989fde8896d988a436574ffa2f2
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-23 18:50:15 +0530

    Hackerrank Solved Problmes

---
+++ Hackerrank/30 Days of Code/Day 10/Binary Numbers.cpp
@@ -0,0 +1,22 @@
+/*
+ * https://www.hackerrank.com/challenges/30-binary-numbers/problem
+ */
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int count = 0;
+
+while (n)
+{
+    n = (n & (n << 1));
+    count++;
+}
+cout<<count;
+
+return 0;
+}
+
