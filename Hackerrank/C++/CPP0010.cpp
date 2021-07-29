2d09a8e217d42bcf7cb4af21fff7306e0b2ad3be
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-29 20:26:56 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/C++/CPP0010.cpp
@@ -0,0 +1,20 @@
+/*
+ * https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+ */
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+ int age;
+ string first_name,last_name,standard;
+}st;
+
+int main() {
+    Student st;
+
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+
+    return 0;
+}
