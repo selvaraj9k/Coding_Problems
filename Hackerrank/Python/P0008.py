1a904cda948011d96d382f46427feef7f642baf2
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-08-09 23:26:05 +0530

    h

--- Hackerrank/Python/P0008.py
+++
@@ -1,9 +0,0 @@
-#https://www.hackerrank.com/challenges/swap-case/problem
-
-def swap_case(s):
-    return s.swapcase()
-
-if __name__ == '__main__':
-    s = input()
-    result = swap_case(s)
-    print(result)
