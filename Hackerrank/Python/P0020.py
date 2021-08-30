08ad42e09a05594835af92a4c62a332ff7ad67b3
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-08-30 18:07:59 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Python/P0020.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+N=int(input())
+list=list(set(map(int,input().strip().split(" "))))
+list.sort(reverse=True)
+print (list[1])
