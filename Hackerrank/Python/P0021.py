6b5f74d0fbc05ce3a1e23eb1bde6cdd8e6fb115b
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-04 22:40:16 +0530

    Hackerrank Solved Problems

--- Hackerrank/Python/P0021.py
+++ Hackerrank/Python/P0021.py
@@ -1,2 +1,9 @@
 # python code
-print("network dead!")
+#https://www.hackerrank.com/challenges/input/problem
+
+x, k = map(int,input().split())
+p= eval(input())
+if(p == k):
+    print("True")
+else:
+    print("False")
