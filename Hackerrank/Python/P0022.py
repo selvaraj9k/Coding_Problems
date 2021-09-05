122653855c1fa06b2a167acd5b6c2af968546c56
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-05 23:21:00 +0530

    Hackerrank Solved Probelms

---
+++ Hackerrank/Python/P0022.py
@@ -0,0 +1,7 @@
+https://www.hackerrank.com/challenges/itertools-product/problem
+
+from itertools import product
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(*product(a, b))
