344c9246e077cc8500949dc8a0780393a15d6577
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-08 19:18:36 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Python/P0024.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/exceptions/problem
+
+for i in range(int(input())):
+    try:
+        a,b=map(int,input().split())
+        print(a//b)
+    except Exception as e:
+        print("Error Code:",e)
