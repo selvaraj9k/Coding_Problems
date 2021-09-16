dae32211e8c6159c6e0c9369399dff64638dd851
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-16 21:17:59 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Python/P0025.py
@@ -0,0 +1,11 @@
+#https://www.hackerrank.com/challenges/incorrect-regex/problem
+
+import re
+n = int(input())
+for i in range(n):
+    string = input()
+    try:
+        regex = re.compile(string)
+        print(True)
+    except re.error:
+        print(False)
