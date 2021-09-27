b3a5c4d6bb39f4e3849890497cfe91a2f915ae86
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-27 18:33:42 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Algorithms/A0007.c
@@ -0,0 +1,22 @@
+/*
+ * https://www.hackerrank.com/challenges/staircase/problem
+ */
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    printf("%*s",n-i,"#");
+    for(j=0;j<i;j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    }
+    return 0;
+}
+
+ 
