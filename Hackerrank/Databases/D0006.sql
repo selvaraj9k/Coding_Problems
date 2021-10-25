e38329060f612b701e8d54a61045ad903d510ba6
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-10-25 21:48:02 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Databases/D0006.sql
@@ -0,0 +1,11 @@
+/*
+https://www.hackerrank.com/challenges/basics-of-sets-and-relational-algebra-7/problem
+*/
+
+SELECT *
+FROM Student INNER JOIN Teaching Assistants ON Student.Number = "Teaching Assistants".ID
+
+/*
+Answer: 2
+*/
+
