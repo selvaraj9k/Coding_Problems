b5b11b572f6a408537abdebd622e864054a4aaee
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-10-30 20:41:05 +0530

    Hakcerrank Solved Problems

---
+++ Hackerrank/10 Days of Javascript/Day 1/ Let and Const.js
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/js10-let-and-const/problem
+
+
+function main() {
+
+    let r = readLine();
+    const PI = Math.PI;
+    
+    console.log(PI*r*r);
+    console.log(2*PI*r);
+}
