4141a2ef7a8fb60851b115f8a3df7660b046649c
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-11-02 20:19:03 +0530

    Hackerrank Solved problems

---
+++ Hackerrank/10 Days of Javascript/Day 3/Try, Catch, and Finally.js
@@ -0,0 +1,15 @@
+//https://www.hackerrank.com/challenges/js10-try-catch-and-finally/problem
+
+
+function reverseString(s) {
+
+  try{
+    console.log(s.split("").reverse().join(""))
+  }
+  catch(e){
+    console.log(e.message);
+    console.log(s);
+  }
+
+
+}
