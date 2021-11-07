9ef6a18fcd7c7b558fcd8d0c9eadb3b927dec84f
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-11-07 20:29:18 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/10 Days of Javascript/Create a Rectangle Object.js
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/js10-objects/problem
+
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a*b
+    }
+
+}
