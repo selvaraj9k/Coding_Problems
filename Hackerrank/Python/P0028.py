dd3889e0512df0de9e7ea20970bbe31b43f6aa29
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-11-17 22:15:59 +0530

    Hackerrank Solved Problems

---
+++ Hackerrank/Python/P0028.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+
+
+def average(array):
+    heights = set(array)
+    averg = sum(heights) / len(heights)
+    return averg
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
