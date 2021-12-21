81e9828f550c2aad2a9c05691c1ce48f70cfaf86
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-12-21 21:48:33 +0530

    Hackerrank Solved Problems

--- Hackerrank/Bash/B051.bash
+++ Hackerrank/Bash/B051.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 #https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-3/problem
 
 
---
+++ Hackerrank/Bash/B053.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+#https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-1/problem
+
+sed -e 's/\bthe\b/this/'
+
