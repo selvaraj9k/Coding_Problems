f5b221df7f23da3e106828ba0cef6affb5f9ac23
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-01-02 22:45:29 +0530

    Hackerrank Solved Problems

--- Hackerrank/Python/P0030.py
+++ Hackerrank/Python/P0030.py
@@ -1,10 +1,8 @@
 #https://www.hackerrank.com/challenges/np-arrays/problem
 
 import numpy
-
 def arrays(arr):
     return numpy.array(arr[::-1], float);
-
 arr = input().strip().split(' ')
 result = arrays(arr)
 print(result)
