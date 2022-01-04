14b692dee7d56123cc4d957ae400cdeb56d60d7d
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-01-04 21:47:39 +0530

    Hackerrank Solved Problems

--- Hackerrank/Bash/B064.bash
+++ Hackerrank/Bash/B064.bash
@@ -1,2 +1,19 @@
 #!/bin/bash
+#https://www.hackerrank.com/challenges/lonely-integer-2/problem
 
+read n
+read -a array
+
+for number in "${array[@]}"; do
+  count=0
+
+  for number2 in "${array[@]}"; do
+    if [ $number2 -eq $number ]; then
+      count=$(( $count + 1 ))
+    fi
+  done
+
+  if [ $count -eq 1 ]; then
+    echo $number
+  fi
+done
